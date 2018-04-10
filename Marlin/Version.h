6ecb033a1e5145912ac76b2ef29dc8949b6791db
Miguel Almeida <ma@blockstec.com>
2018-04-10 10:28:20 +0100

    Remove `MK2` from printer name

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -70,7 +70,7 @@
   /**
    * Defines a generic printer name to be output to the LCD after booting Marlin.
    */
-  #define MACHINE_NAME "Blocks One MK2"
+  #define MACHINE_NAME "Blocks One"
 
   /**
    * The SOURCE_CODE_URL is the location where users will find the Marlin Source
